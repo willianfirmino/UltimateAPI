ff060cb416becd676902e926d70c0889e13c042c
William Firmino <willian.firmino1@gmail.com>
2023-03-02 16:44:56 -0300

    Added companies DTOs

--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -1,6 +1,7 @@
 using Contracts;
 using Entities.Models;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 
 namespace Service;
 
@@ -14,16 +15,17 @@ public sealed class CompanyService : ICompanyService
         _repository = repository;
     }
 
-    public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+    public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
     {
         try
         {
             var companies = _repository.Company.GetAllCompanies(trackChanges);
-            return companies;
+            var companiesDto = companies.Select(c =>
+                new CompanyDto(c.Id, c.Name ?? "", string.Join(' ', c.Address, c.Country))).ToList();
+            return companiesDto;
         }
         catch (Exception ex)
         {
-
             _logger.LogError($"Something went wrong in the {nameof(GetAllCompanies)} service method {ex}");
             throw;
         }
